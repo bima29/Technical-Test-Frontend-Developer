9b4c851e3247a456e2fdc7ad1b2943b278903a0b
Dwihadi Bima Pangestu <94498243+bima29@users.noreply.github.com>
2025-10-18 16:47:31 +0700

    update
    update

--- src/components/KanbanView.vue
+++ src/components/KanbanView.vue
@@ -281,11 +281,6 @@ defineExpose({ openModal })
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"/>
               </svg>
             </button>
-            <button class="p-1 text-gray-400 hover:text-gray-200 ml-auto transition-colors">
-              <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"/>
-              </svg>
-            </button>
           </div>
         </div>
 
